a0c8ea758bc872d6265a7609d2f774ebd62c6c5a
Luis Miguel F <miguelfr.reg@gmail.com>
2016-11-01 19:09:06 +0100

    added more recipes

--- src/data/recipes_friday.js
+++ src/data/recipes_friday.js
@@ -1,9 +1,209 @@
 
 export default {
     "food": [
-
+        {
+            id: 5101,
+            title: "Patatas con carne",
+            photo: "http://www.cocinasincarne.es/wp-content/uploads/2013/02/Patatas-con-carne7.jpg",
+            portion: 2.5,
+            explainedPortion: "2,5 Vasos llenos",
+            dessert: "Yogur desnatado",
+            severalDishes: false,
+            accompaniments: [],
+            forPortions: 4,
+            time: "30min",
+            category: "Fácil",
+            ingredients: [
+                "200gr. Vacuno (bistec magro)",
+                "80gr. Zanahorias",
+                "100gr. Cebollas",
+                "25gr. Ajos (5 dientes)",
+                "510gr. Patatas",
+                "10gr. Aceite oliva",
+                "Perejil",
+                "Laurel (1 hoja)",
+                "Pimentón (1 cucharadita)",
+                "Sal"
+            ],
+            elaboration: [
+                "Se ponen todos los ingredientes en la olla rápida, una vez limpios y " +
+                "troceados. Se deja cocer de 20 a 25 minutos. ",
+                "Hasta que se vea cocico, si se hace en cazuela tradicional, añadiendo agua " +
+                "de vez en cuando si es necesario."
+            ]
+        },{
+            id: 5102,
+            title: "Vinagreta sin tomate",
+            photo: "http://www.cameroonpostline.com/wp-content/uploads/2015/08/no-image-available2.png",
+            portion: 1,
+            explainedPortion: "La receta completa",
+            dessert: "Yogur desnatado",
+            severalDishes: false,
+            accompaniments: [],
+            forPortions: 1,
+            time: "20min",
+            category: "Fácil",
+            ingredients: [
+                "125gr. Patatas",
+                "80gr. Atún claro al natural",
+                "24gr. Aceituna",
+                "90gr. Lechuga",
+                "100gr. Espárragos blancos",
+                "40gr. Zanahorias",
+                "50gr. Huevo (1 pequeño)",
+                "10gr. Aceite oliva (1 cucharada)",
+                "Vinagre (al gusto)",
+                "Sal"
+            ],
+            elaboration: [
+                "Cocer la patata, la zanahoria y el huevo. Una vez fríos, se pelan y trocean " +
+                "en un plato o ensaladera. Se lava y corta en juliana la lechuga. Se añaden " +
+                "el atún, los espárragos y las aceitunas. Se aliña con una cucharada de aceite " +
+                "de oliva, vinagre y sal.",
+                "El atún puede ser en escabeche"
+            ]
+        },{
+            id: 5103,
+            title: "Vinagreta sin huevo",
+            photo: "http://www.cameroonpostline.com/wp-content/uploads/2015/08/no-image-available2.png",
+            portion: 1,
+            explainedPortion: "La receta completa",
+            dessert: "Yogur desnatado",
+            severalDishes: false,
+            accompaniments: [],
+            forPortions: 1,
+            time: "20min",
+            category: "Fácil",
+            ingredients: [
+                "125gr. Patatas",
+                "80gr. Atún claro al natural",
+                "90gr. Lechuga",
+                "100gr. Espárragos blancos",
+                "100gr. Tomates",
+                "60gr. Pimiento piquillo (3)",
+                "10gr. Aceite oliva (1 cucharada)",
+                "Vinagre (al gusto)",
+                "Sal"
+            ],
+            elaboration: [
+                "Cocer la patata. Una vez fría, se pela y trocea en un plato o ensaladera. Se " +
+                "lava y corta en juliana la lechuga. Se añaden el tomate, el atún, los espárragos " +
+                "y los pimientos. Se aliña con una cucharada de aceite de oliva, vinagre y sal.",
+                "El atún puede ser en escabeche"
+            ]
+        },{
+            id: 5104,
+            title: "Patatas en salsa verde",
+            photo: "http://1.bp.blogspot.com/-O8_YnXHXTZk/T9z0hqYj7mI/AAAAAAAAAPM/rBZnY6qcwqo/s1600/DSC04030.JPG",
+            portion: 2.5,
+            explainedPortion: "2,5 vasos llenos hasta el borde",
+            dessert: "Yogur desnatado",
+            severalDishes: false,
+            accompaniments: [],
+            forPortions: 4,
+            time: "30min",
+            category: "Fácil",
+            ingredients: [
+                "480gr. Patatas",
+                "75gr. Cebollas",
+                "140gr. Guisantes",
+                "10gr. Ajos (2 dientes)",
+                "20gr. Aceite oliva (2 cucharadas)",
+                "Perejil",
+                "Sal"
+            ],
+            elaboration: [
+                "Se ponen todos los ingredientes en la olla rápida, una vez limpios y troceados.",
+                "Se deja cocer de 15 a 20 minutos.",
+                "OPCIONES: Se pueden cambiar las 2,5 raciones por 1,5 raciones y acompañar con 100gr. " +
+                "de filete de ternera a la plancha."
+            ]
+        },{
+            id: 5105,
+            title: "Marmita de bonito",
+            photo: "http://www.spain365.es/sites/default/files/styles/blog_full/public/field/image/blog_post/marmitako.jpg?itok=4OCnwKIg",
+            portion: 2.5,
+            explainedPortion: "La mitad de la receta",
+            dessert: "Yogur desnatado",
+            severalDishes: false,
+            accompaniments: [],
+            forPortions: 5,
+            time: "30min",
+            category: "Fácil",
+            ingredients: [
+                "510gr. Patatas",
+                "80gr. Zanahorias",
+                "100gr. Cebollas",
+                "200gr. Bonito",
+                "25gr. Ajos (5 dientes)",
+                "10gr. Aceite oliva (1 cucharada)",
+                "10gr. Perejil",
+                "Laurel (1 hoja)",
+                "Pimentón (1 cucharadita)",
+                "Sal"
+            ],
+            elaboration: [
+                "Se ponen todos los ingredientes en la olla rápida, una vez limpios y troceados. " +
+                "Se deja cocer de 20 a 25 minutos.",
+                "Hasta que se vea cocico, si se hace en cazuela tradicional, añadiendo agua " +
+                "de vez en cuando si es necesario."
+            ]
+        }
     ],
     "dinner": [
-
+        {
+            id: 5201,
+            title: "Dorada a la plancha",
+            photo: "http://www.recetas-faciles.org/wp-content/uploads/dorada.jpg",
+            portion: 1,
+            explainedPortion: "Receta completa",
+            dessert: "1 Manzana",
+            severalDishes: true,
+            accompaniments: [5202],
+            forPortions: 1,
+            time: "15min",
+            category: "Fácil",
+            ingredients: [
+                "200gr. Dorada (en limpio)",
+                "5gr. Aceite oliva",
+                "5gr. Ajos (epicado o en polvo)",
+                "Perejil",
+                "Vinagre",
+                "Sal"
+            ],
+            elaboration: [
+                "Humedecer la plancha con un poco de aceite. Salar un poco la ración de dorada " +
+                "con sal gruesa. Será media dorada o una de ración, según tamaño, y estará abierta " +
+                "a la espalda. Ponerla por el lado de la piel en la plancha cuando haya calentado. " +
+                "Se vierte si se quiere un chorrito de vinagre de vino, y se espolvorea con ajo en " +
+                "polvo o picado y perejil. Cuando se haya hecho por el lado de la piel, se la da la " +
+                "vuelta para hacer el lado condimentado."
+            ]
+        },{
+            id: 5202,
+            title: "Ensalada de lechuga, tomate, pimiento y espárragos",
+            photo: "http://www.cameroonpostline.com/wp-content/uploads/2015/08/no-image-available2.png",
+            portion: 1,
+            explainedPortion: "Receta completa",
+            dessert: "1 Manzana",
+            severalDishes: true,
+            accompaniments: [5201],
+            forPortions: 1,
+            time: "15min",
+            category: "Fácil",
+            ingredients: [
+                "90gr. Lechuga",
+                "150gr. Tomates",
+                "100gr. Espárragos",
+                "70gr. Pimientos verdes",
+                "5gr. Aceite oliva (1 cucharadita)",
+                "Vinagre",
+                "Sal"
+            ],
+            elaboration: [
+                "Lavar y cortar la lechuga, el tomate y el resto de ingredientes. Aliñar " +
+                "con el aceite, el vinagre y la sal."
+            ]
+        }
     ]
 }
